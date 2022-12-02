3460983e0a3213edfc7ad5569c7a583ab4ef0cf9
tshipenchko <dev@zxc.sx>
2022-12-02 22:07:28 +0600

    Add registration of new users

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -2,10 +2,18 @@ from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart
 from fluentogram import TranslatorRunner
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from bot.db.models import User
 
 router = Router()
 
 
 @router.message(CommandStart())
-async def start(message: Message, i18n: TranslatorRunner):
+async def start(message: Message, i18n: TranslatorRunner, session: AsyncSession):
+    async with session.begin():
+        await session.merge(
+            User(id=message.from_user.id)
+        )
+
     await message.answer(i18n.start())
