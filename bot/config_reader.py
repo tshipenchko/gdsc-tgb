62ebc6c61abd14c1e20bf704df841ee35ee973cf
tshipenchko <dev@zxc.sx>
2022-12-08 13:29:26 +0600

    Docker and docker-compose integration ** breaking changes: config_reader.py

--- bot/config_reader.py
+++ bot/config_reader.py
@@ -3,9 +3,20 @@ from pydantic import BaseSettings, SecretStr
 
 class Settings(BaseSettings):
     bot_token: SecretStr
-    db_url: str
+    db_user: str
+    db_password: SecretStr
+    db_name: str
+    db_host: str
+    db_port: int
 
     root_locale: str = "en"
+
+    @property
+    def db_url(self):
+        return f"postgresql+asyncpg://" \
+               f"{self.db_user}:{self.db_password.get_secret_value()}" \
+               f"@" \
+               f"{self.db_host}:{self.db_port}/{self.db_name}"
 
     class Config:
         env_file = ".env"
