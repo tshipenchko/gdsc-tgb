33c8c173463455d82674f84989c93dd0b8f202ca
misteralen <alen.sultanov@gmail.com>
2022-12-16 11:07:23 +0600

    closes #14

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -1,14 +1,22 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from fluentogram import TranslatorRunner
 
 
-def add_menu(is_auth: bool):
+def add_menu(is_auth: bool, i18n: TranslatorRunner):
     kb = InlineKeyboardBuilder()
     if not is_auth:
-        kb.button(text="Login", callback_data="login")
+        kb.button(text=i18n.login_btn(), callback_data="login")
     else:
-        kb.button(text="Profile", callback_data="profile")
-        kb.button(text="Events", callback_data="events")
-        kb.button(text="Info", callback_data="info")
+        kb.button(text=i18n.profile_btn(), callback_data="profile")
+        kb.button(text=i18n.events_btn(), callback_data="events")
+        kb.button(text=i18n.info_btn(), callback_data="info")
         kb.adjust(1, 2)
+
+    return kb.as_markup()
+
+
+def add_back_btn(i18n: TranslatorRunner):
+    kb = InlineKeyboardBuilder()
+    kb.button(text=i18n.back_to_menu_btn(), callback_data="menu")
 
     return kb.as_markup()
--- bot/locales/en/main.ftl
+++ bot/locales/en/main.ftl
@@ -1,3 +1,13 @@
 start = Choose one and click:
-login = Click the button to authorise
+login_text = Click the button to authorise
 login_success = You successfully logged in
+
+profile_text = Profile
+events_text = Events
+info_text = Information
+
+login_btn = Login
+profile_btn = Profile
+events_btn = Events
+info_btn = Info
+back_to_menu_btn = Back
